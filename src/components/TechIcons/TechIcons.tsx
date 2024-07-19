0bb3dad1085cfa5600bf10f4ba48e1efbb714296
gnagetEmil <emil@gnaget.se>
2024-07-19 18:16:23 +0200

    Update Skills and TechIcons components: add TypeScript icon, adjust layout and formatting

--- src/components/TechIcons/TechIcons.tsx
+++ src/components/TechIcons/TechIcons.tsx
@@ -1,27 +1,36 @@
-import { Box, Image, Tooltip, Wrap, WrapItem } from '@chakra-ui/react';
+import { Box, Image, Tooltip, Wrap, WrapItem } from "@chakra-ui/react";
 
 const TechIcons = () => {
   const icons = [
-    { src: './images/JavaScript.png', alt: 'JavaScript' },
-    { src: './images/React.png', alt: 'React' },
-    { src: './images/Nodejs.png', alt: 'Node.js' },
-    { src: './images/Python.png', alt: 'Python' },
-    { src: './images/MySQL.png', alt: 'MySQL' },
-    { src: './images/MongoDB.png', alt: 'MongoDB' },
-    { src: './images/Git.png', alt: 'Git' },
-    { src: './images/Jira.png', alt: 'Jira' },
-    { src: './images/Bootstrap.png', alt: 'Bootstrap' },
-    { src: './images/Kotlin.png', alt: 'Kotlin' },
-    { src: './images/Java.png', alt: 'Java' },
+    { src: "./images/JavaScript.png", alt: "JavaScript" },
+    { src: "./images/TypeScript.png", alt: "TypeScript" },
+    { src: "./images/React.png", alt: "React & React Native" },
+    { src: "./images/Bootstrap.png", alt: "Bootstrap" },
+    { src: "./images/Nodejs.png", alt: "Node.js" },
+    { src: "./images/Python.png", alt: "Python" },
+    { src: "./images/Java.png", alt: "Java" },
+    { src: "./images/Kotlin.png", alt: "Kotlin" },
+    { src: "./images/MySQL.png", alt: "MySQL" },
+    { src: "./images/MongoDB.png", alt: "MongoDB" },
+    { src: "./images/Git.png", alt: "Git" },
+    { src: "./images/Jira.png", alt: "Jira" },
   ];
 
   return (
-    <Wrap spacing={'30'} justify="center" maxW={600}>
+    <Wrap spacing={"30"} justify="center" maxW={600}>
       {icons.map((icon, index) => (
         <Tooltip label={icon.alt} key={index} hasArrow>
           <WrapItem>
-            <Box p="4" boxShadow='base' rounded='md' bg="background" borderRadius="50"> {/* Updated background color */}
-              <Image src={icon.src} alt={icon.alt} boxSize={8}/>
+            <Box
+              p="4"
+              boxShadow="base"
+              rounded="md"
+              bg="background"
+              borderRadius="50"
+            >
+              {" "}
+              {/* Updated background color */}
+              <Image src={icon.src} alt={icon.alt} boxSize={8} />
             </Box>
           </WrapItem>
         </Tooltip>
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,44 +1,63 @@
-import { Box, Container, Flex, Heading, chakra, Text, Image } from '@chakra-ui/react';
-import Technologies from '../../components/TechIcons/TechIcons';
-import { FaScrewdriverWrench } from 'react-icons/fa6';
-import { useTranslation } from 'react-i18next';
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  chakra,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import Technologies from "../../components/TechIcons/TechIcons";
+import { FaScrewdriverWrench } from "react-icons/fa6";
+import { useTranslation } from "react-i18next";
 
 function Skills() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <Container maxWidth="1200px" marginTop="200px">
-            <Heading size="lg" color='text'>
-                <chakra.span
-                    borderBottom="3px solid"
-                    borderColor={'accent'}
-                    display="inline-block"
-                    paddingBottom="4px"
-                >
-                    <Flex>
-                        <Box mt={1}><FaScrewdriverWrench color="text" /></Box>
-                        <Text ml={2} color="text">{t('Technologies and Tools Title')}</Text>
-                    </Flex>
-                </chakra.span>
-            </Heading>
+  return (
+    <Container maxWidth="1200px" marginTop="200px">
+      <Heading size="lg" color="text">
+        <chakra.span
+          borderBottom="3px solid"
+          borderColor={"accent"}
+          display="inline-block"
+          paddingBottom="4px"
+        >
+          <Flex>
+            <Box mt={1}>
+              <FaScrewdriverWrench color="text" />
+            </Box>
+            <Text ml={2} color="text">
+              {t("Technologies and Tools Title")}
+            </Text>
+          </Flex>
+        </chakra.span>
+      </Heading>
 
-            <Flex 
-                direction={{ base: "column", xl: "row" }}
-                wrap="wrap"
-                justifyContent={{ base: "center", md: "space-between" }}
-                mt={4}
-                alignItems="center"
-            >
-                <Image mt={10} mb={10} src='/images/Skills.gif' alt='Skills' maxBlockSize="400px" objectFit="cover" />
-                <Box color='text' textAlign={'center'} mt={{ base: 0, md: 10 }}>
-                    <Technologies />
-                    <Text mt={10}>{t('Skills 1')}</Text>
-                    <Text>{t('Skills 2')}</Text>
-                    <Text>{t('Skills 3')}</Text>
-                </Box>
-            </Flex>
-        </Container>
-    );
+      <Flex
+        direction={{ base: "column", xl: "row" }}
+        wrap="wrap"
+        justifyContent={{ base: "center", md: "space-between" }}
+        mt={4}
+        alignItems="center"
+      >
+        <Image
+          mt={10}
+          mb={10}
+          src="/images/Skills.gif"
+          alt="Skills"
+          maxBlockSize="400px"
+          objectFit="cover"
+        />
+        <Box color="text" textAlign={"center"} mt={{ base: 0, md: 0 }}>
+          <Technologies />
+          <Text mt={10}>{t("Skills 1")}</Text>
+          <Text>{t("Skills 2")}</Text>
+          <Text>{t("Skills 3")}</Text>
+        </Box>
+      </Flex>
+    </Container>
+  );
 }
 
 export default Skills;
