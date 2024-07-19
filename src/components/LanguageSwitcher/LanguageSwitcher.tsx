ed7cfbe58e053103319af00e30dccc900a4dc1bc
gnagetEmil <emil@gnaget.se>
2024-07-19 21:49:35 +0200

    Update Navbar and LanguageSwitcher components
    - Refactor Navbar component for improved styling and structure.
    - Enhance LanguageSwitcher component with better hover effects and text color.

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,6 +1,16 @@
-import { Menu, MenuButton, MenuList, MenuItem, Image, Box, Flex, Text } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import { useTranslation } from 'react-i18next';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Image,
+  Box,
+  Flex,
+  Text,
+  background,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { useTranslation } from "react-i18next";
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
@@ -12,9 +22,9 @@ const LanguageSwitcher = () => {
 
   const getFlagByLang = (lang: string) => {
     switch (lang) {
-      case 'sv':
+      case "sv":
         return "https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg";
-      case 'en':
+      case "en":
       default:
         return "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg";
     }
@@ -22,41 +32,73 @@ const LanguageSwitcher = () => {
 
   return (
     <Menu>
-            <MenuButton mr={5} _hover={{
-                textDecoration: 'underline',
-                textDecorationColor: 'accent',
-                color: 'text',
-                textDecorationThickness: '2px'
-                }}>
-            <Box as="span" display="flex" alignItems="center">
-            <Image boxSize={5} src={getFlagByLang(currentLanguage)} alt='Current Language' mr={1} mt={[0, 1]}  />
-            Language
-              <ChevronDownIcon boxSize={6}></ChevronDownIcon>
-              </Box>
-            </MenuButton>
-            <MenuList color='text' bg={'background'}>
+      <MenuButton
+        mr={5}
+        _hover={{
+          textDecoration: "underline",
+          textDecorationColor: "background",
+          color: "text",
+          textDecorationThickness: "2px",
+        }}
+      >
+        <Box as="span" display="flex" alignItems="center">
+          <Image
+            boxSize={5}
+            src={getFlagByLang(currentLanguage)}
+            alt="Current Language"
+            mr={1}
+            mt={[0, 1]}
+          />
+          <Text color={"background"}>Language</Text>
+          <ChevronDownIcon boxSize={6} color={"Background"}></ChevronDownIcon>
+        </Box>
+      </MenuButton>
+      <MenuList color="text" bg={"background"}>
+        <Text
+          onClick={() => changeLanguage("en")}
+          _hover={{
+            textDecoration: "underline",
+            textDecorationColor: "text",
+            textDecorationThickness: "2px",
+          }}
+        >
+          <MenuItem bg={"background"}>
+            <Flex marginEnd={2}>
+              {" "}
+              <Image
+                boxSize={5}
+                src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
+                alt="USA Flag"
+                mt={[0, 1]}
+              />
+            </Flex>
+            <Text>English</Text>
+          </MenuItem>
+        </Text>
 
-                <Text onClick={() => changeLanguage('en')} _hover={{
-                textDecoration: 'underline',
-                textDecorationColor: 'accent',
-                textDecorationThickness: '2px'
-                }}>
-
-                    <MenuItem bg={'background'}><Flex marginEnd={2}> <Image boxSize={5} src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg" alt="USA Flag"   mt={[0, 1]}  /></Flex><Text>English</Text></MenuItem>
-                
-                </Text>
-
-                <Text onClick={() => changeLanguage('sv')} _hover={{
-                textDecoration: 'underline',
-                textDecorationColor: 'accent',
-                textDecorationThickness: '2px'
-                }}>
-                    <MenuItem bg={'background'}><Flex marginEnd={2}><Image boxSize={5} src="https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg" alt="Swedish Flag"  mt={[0, 1]}  /></Flex><Text>Svenska</Text></MenuItem> 
-                </Text>
-            
-            </MenuList>
-          </Menu>
+        <Text
+          onClick={() => changeLanguage("sv")}
+          _hover={{
+            textDecoration: "underline",
+            textDecorationColor: "text",
+            textDecorationThickness: "2px",
+          }}
+        >
+          <MenuItem bg={"background"}>
+            <Flex marginEnd={2}>
+              <Image
+                boxSize={5}
+                src="https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg"
+                alt="Swedish Flag"
+                mt={[0, 1]}
+              />
+            </Flex>
+            <Text>Svenska</Text>
+          </MenuItem>
+        </Text>
+      </MenuList>
+    </Menu>
   );
-}
+};
 
 export default LanguageSwitcher;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -36,140 +36,146 @@ const Navbar = () => {
   ];
 
   return (
-    <Container
-      borderBottom="1px solid"
-      borderColor={"text"}
-      maxWidth={"1400px"}
-      color="text"
-      position="fixed"
+    <Box
+      w="100%"
+      position="absolute"
       top="0"
       left="0"
-      right="0"
       zIndex="1000"
-      bg="background"
+      bg="primary"
     >
-      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon boxSize="24px" />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-          variant="ghost"
-          backgroundColor="transparent"
-          _hover={{ bg: "transparent" }}
-          _active={{ bg: "transparent" }}
-          _focus={{ boxShadow: "none" }}
-          color="text"
-        />
-        <HStack spacing={8} alignItems={"center"}>
-          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-            {Links.map((link, index) => (
-              <Box
-                key={index}
-                as="a"
-                href={`#${link.ref}`}
-                p={2}
-                rounded={"md"}
-                _hover={{
-                  textDecoration: "underline",
-                  textDecorationColor: "accent",
-                  textDecorationThickness: "2px",
-                  color: "text",
-                }}
-                color="text"
-              >
-                {link.name}
-              </Box>
-            ))}
+      <Container maxW="container.xl">
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <IconButton
+            size="md"
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon boxSize="24px" />}
+            aria-label={isOpen ? "Close Menu" : "Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+            variant="ghost"
+            backgroundColor="transparent"
+            _hover={{ bg: "transparent" }}
+            _active={{ bg: "transparent" }}
+            _focus={{ boxShadow: "none" }}
+            color="background"
+          />
+          <HStack spacing={8} alignItems="center">
+            <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
+              {Links.map((link, index) => (
+                <Box
+                  key={index}
+                  as="a"
+                  href={`#${link.ref}`}
+                  p={2}
+                  rounded="md"
+                  _hover={{
+                    textDecoration: "underline",
+                    textDecorationColor: "background",
+                    textDecorationThickness: "2px",
+                    color: "background",
+                  }}
+                  color="background"
+                  fontWeight="medium"
+                  fontSize="md"
+                >
+                  {link.name}
+                </Box>
+              ))}
+            </HStack>
           </HStack>
-        </HStack>
-        <Flex alignItems={"center"}>
-          <LanguageSwitcher />
-          <Menu>
-            <MenuButton>
-              <Avatar
-                size="sm"
-                name="Emil Jacobsson"
-                src="/images/Profilbild.jpeg"
-              />
-              <ChevronDownIcon boxSize={6} mt={1}></ChevronDownIcon>
-            </MenuButton>
-            <MenuList color="text" bg={"background"}>
-              <Link
-                href="https://www.linkedin.com/in/emiljacobsson/"
-                isExternal
-                _hover={{
-                  textDecoration: "underline",
-                  textDecorationColor: "accent",
-                  textDecorationThickness: "2px",
-                }}
-              >
-                <MenuItem bg={"background"}>
-                  <Flex marginEnd={2}>
-                    <FaLinkedin color="#0A66C2" />
-                  </Flex>
-                  <Text>{t("Linkedin")}</Text>
-                </MenuItem>
-              </Link>
-              <Link
-                href="https://github.com/JacobssonEmil"
-                isExternal
-                _hover={{
-                  textDecoration: "underline",
-                  textDecorationColor: "accent",
-                  textDecorationThickness: "2px",
-                }}
-              >
-                <MenuItem bg={"background"}>
-                  <Flex marginEnd={2}>
-                    <FaGithub color="#181717" />
-                  </Flex>
-                  <Text>{t("Github")}</Text>
-                </MenuItem>
-              </Link>
-              <Link
-                href="mailto:emil@jacobsson.com"
-                isExternal
-                _hover={{
-                  textDecoration: "underline",
-                  textDecorationColor: "accent",
-                  textDecorationThickness: "2px",
-                }}
-              >
-                <MenuItem bg={"background"}>
-                  <EmailIcon marginTop={1} marginEnd={2} color="text" />
-                  <Text>{t("Email")}</Text>
-                </MenuItem>
-              </Link>
-            </MenuList>
-          </Menu>
+          <Flex alignItems="center">
+            <LanguageSwitcher />
+            <Menu>
+              <MenuButton _hover={{ bg: "gray.700" }}>
+                <Avatar
+                  size="sm"
+                  name="Emil Jacobsson"
+                  src="/images/Profilbild.jpeg"
+                />
+                <ChevronDownIcon boxSize={6} mt={1} color="background" />
+              </MenuButton>
+              <MenuList color="text" bg="background">
+                <Link
+                  href="https://www.linkedin.com/in/emiljacobsson/"
+                  isExternal
+                  _hover={{
+                    textDecoration: "underline",
+                    textDecorationColor: "text",
+                    color: "text",
+                    textDecorationThickness: "2px",
+                  }}
+                >
+                  <MenuItem bg="white">
+                    <Flex marginEnd={2}>
+                      <FaLinkedin color="#0A66C2" />
+                    </Flex>
+                    <Text>{t("Linkedin")}</Text>
+                  </MenuItem>
+                </Link>
+                <Link
+                  href="https://github.com/JacobssonEmil"
+                  isExternal
+                  _hover={{
+                    textDecoration: "underline",
+                    textDecorationColor: "text",
+                    color: "text",
+                    textDecorationThickness: "2px",
+                  }}
+                >
+                  <MenuItem bg="white">
+                    <Flex marginEnd={2}>
+                      <FaGithub color="text" />
+                    </Flex>
+                    <Text>{t("Github")}</Text>
+                  </MenuItem>
+                </Link>
+                <Link
+                  href="mailto:emil@jacobsson.com"
+                  isExternal
+                  _hover={{
+                    textDecoration: "underline",
+                    textDecorationColor: "text",
+                    color: "text",
+                    textDecorationThickness: "2px",
+                  }}
+                >
+                  <MenuItem bg="white">
+                    <EmailIcon marginTop={1} marginEnd={2} color="text" />
+                    <Text>{t("Email")}</Text>
+                  </MenuItem>
+                </Link>
+              </MenuList>
+            </Menu>
+          </Flex>
         </Flex>
-      </Flex>
-      {isOpen && (
-        <Box pb={4} display={{ md: "none" }}>
-          <Stack as={"nav"} spacing={4}>
-            {Links.map((link, index) => (
-              <Box
-                key={index}
-                as="a"
-                href={`#${link.ref}`}
-                p={2}
-                rounded={"md"}
-                _hover={{
-                  textDecoration: "underline",
-                  textDecorationColor: "accent",
-                  textDecorationThickness: "2px",
-                }}
-                color="text"
-              >
-                {link.name}
-              </Box>
-            ))}
-          </Stack>
-        </Box>
-      )}
-    </Container>
+        {isOpen && (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as="nav" spacing={4}>
+              {Links.map((link, index) => (
+                <Box
+                  key={index}
+                  as="a"
+                  href={`#${link.ref}`}
+                  p={2}
+                  rounded="md"
+                  _hover={{
+                    textDecoration: "underline",
+                    textDecorationColor: "white",
+                    textDecorationThickness: "2px",
+                    color: "white",
+                  }}
+                  color="white"
+                  fontWeight="medium"
+                  fontSize="md"
+                >
+                  {link.name}
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+        )}
+      </Container>
+    </Box>
   );
 };
 
